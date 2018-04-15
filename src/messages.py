f358dc2108fca97b8d14f1d060338834df9cd157
dustinface <xdustinfacex@gmail.com>
2018-04-15 14:22:01 +0200

    Updated help message

--- src/messages.py
+++ src/messages.py
@@ -124,8 +124,8 @@ def help(messenger):
                 "<cb>detail<ca> - List all details of your SmartNodes\n"
                 "<cb>nodes<ca> - List only the status and last payments of your nodes\n"
                 "<cb>top<ca> <b>:filter<b> - List only the eligible nodes in the payout zone (Top 10% of the queue) per default. You can use the optional <b>:filter<b> argument to list other ranges (0 - 100).\n"
-                "  <b>Example<b>: <b>top 20<b> Will show you all your nodes in the top 20% of the queue - \n"
-                "  <b>Example<b>: <b>top 40<b> Will show you all your nodes in the top 40% of the queue - \n"
+                "  <b>Examples<b>: <cb>top 20<ca> Will show you all your nodes in the top 20% of the queue - \n"
+                "                  <cb>top 40<ca> Will show you all your nodes in the top 40% of the queue - \n"
                 "<cb>history<ca> - List information about past payouts of your SmartNodes\n\n")
 
     if messenger == 'discord':
